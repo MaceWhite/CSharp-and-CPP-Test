ec15d3b5aff7a3433a74615b98b2bacaaaa4d024
Mace <167714041+MaceWhite@users.noreply.github.com>
2024-07-31 07:00:26 +0000

    Update CPP_Test.hpp

--- CPP_Test.hpp
+++ CPP_Test.hpp
@@ -1,7 +1,7 @@
 #ifndef CPP_TEST_H
 #define CPP_TEST_H
 
-#define DLL_EXPORT __declspec(dllexport) // This exposes created APIs for external use in a DLL file, this is only for Windows, if you're using *nix, you don't need this
+#define DLL_EXPORT __declspec(dllexport) // This exposes created APIs for external use in a DLL file, this is only for Windows, if you're using *nix (MacOS, Linux, FreeBSD, etc) you don't need this
 
 extern "C"
 {
