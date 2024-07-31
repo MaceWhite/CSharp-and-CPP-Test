5f2f084b75d5199ab4f49e7497af3bcf268e6f4b
Mace <167714041+MaceWhite@users.noreply.github.com>
2024-07-31 06:53:39 +0000

    Update CPP_Test.cpp

--- CPP_Test.cpp
+++ CPP_Test.cpp
@@ -5,7 +5,7 @@
    We compile this file into a .dll or .so file. We only compile this file, not the header file.
 */
 
-extern "C" // We say extern "C" to tell the compiler that this isn't 
+extern "C" // We say extern "C" to tell the compiler that this isn't C++ code
 {
    DLL_EXPORT int GenNumberOneToFive() // The actual function
    {
